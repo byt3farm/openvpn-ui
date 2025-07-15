2ed25011b498400f564cb3a88a09da08fd475f95
Damian <damian@bytefarm.ch>
2025-07-16 00:53:02 +0200

    fix indent

--- build/standalone-install.sh
+++ build/standalone-install.sh
@@ -12,7 +12,7 @@ read -p "Do you want to continue? (y/n)" -n 1 -r
 echo    # move to a new line
 if [[ ! $REPLY =~ ^[Yy]$ ]]
 then
-    exit 1
+  exit 1
 fi
 
 # Prevent running as root or via sudo (because go might not be found in PATH when done so)
@@ -30,14 +30,13 @@ fi
 go_version=$(go version 2>/dev/null | awk '{print $3}' | tr -d "go")
 if [[ -z "$go_version" || "$go_version" < $GOVERSION ]]
 then
-    echo "Golang version ${GOVERSION} is not installed."
-    read -p "Would you like to install it? (y/n) " -n 1 -r
-    echo    # move to a new line
-    if [[ $REPLY =~ ^[Yy]$ ]]
-    then
-        # Install Go
-  
-    arch=$(uname -m)
+  echo "Golang version ${GOVERSION} is not installed."
+  read -p "Would you like to install it? (y/n) " -n 1 -r
+  echo    # move to a new line
+  if [[ $REPLY =~ ^[Yy]$ ]]
+  then
+    # Install Go
+    arch=$(uname -m) # detect current target system's architecture
     case "$arch" in
       x86_64)
       goarch="amd64"
@@ -53,29 +52,29 @@ then
       exit 1
       ;;
     esac
-
+  
     gofile="go${GOVERSION}.linux-${goarch}.tar.gz"
     gourl="https://go.dev/dl/${gofile}"
-
+  
     echo "Detected architecture: $arch → downloading $gourl"
-
+  
     wget -q --show-progress "$gourl" || { echo "Failed to download $gofile"; exit 1; }
     sudo tar -C /usr/local -xzf "$gofile"
     rm "$gofile"
 
-        export PATH=$PATH:/usr/local/go/bin
-        echo "export PATH=$PATH:$(go env GOPATH)/bin" >> ~/.bashrc
-        source ~/.bashrc
-    else
-        read -p "Would you like to continue without Golang ${GOVERSION} installation? (y/n) " -n 1 -r
-        echo    # move to a new line
-        if [[ ! $REPLY =~ ^[Yy]$ ]]
-        then
-            echo "Installation terminated by user."
-            exit 1
-        fi
+    export PATH=$PATH:/usr/local/go/bin
+    echo "export PATH=$PATH:$(go env GOPATH)/bin" >> ~/.bashrc
+    source ~/.bashrc
+  else
+    read -p "Would you like to continue without Golang ${GOVERSION} installation? (y/n) " -n 1 -r
+    echo    # move to a new line
+    if [[ ! $REPLY =~ ^[Yy]$ ]]
+    then
+      echo "Installation terminated by user."
+      exit 1
     fi
-else
+  fi
+se
   echo "Go version $go_version found: $(which go)"
 fi
 
@@ -84,9 +83,9 @@ read -p "Would you like to run apt-get update? (y/n) " -n 1 -r
 echo    # move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-    # Update your system
-    echo "Updating current environment with apt-get update"
-    sudo apt-get update -y
+  # Update your system
+  echo "Updating current environment with apt-get update"
+  sudo apt-get update -y
 fi
 
 # Install necessary tools
@@ -94,9 +93,9 @@ read -p "Would you like to install the dependencies (sed, gcc, git, musl-tools, 
 echo    # move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-    # Install necessary tools
-    echo "Installing dependencies (sed, gcc, git, musl-tools, easy-rsa, curl, jq, oathtool)"
-    sudo apt-get install -y sed gcc git musl-tools easy-rsa curl jq oathtool
+  # Install necessary tools
+  echo "Installing dependencies (sed, gcc, git, musl-tools, easy-rsa, curl, jq, oathtool)"
+  sudo apt-get install -y sed gcc git musl-tools easy-rsa curl jq oathtool
 fi
 
 # Go Modules download
@@ -104,18 +103,18 @@ read -p "Would you like to download all necessary Go modules? (y/n) " -n 1 -r
 echo    # move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-    # Download all Go modules
-    echo "Downloading all Go modules (go mod download)"
-    go mod download
+  # Download all Go modules
+  echo "Downloading all Go modules (go mod download)"
+  go mod download
 fi
 
 read -p "Would you like to install Beego v2? (y/n) " -n 1 -r
 echo    # move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-    # Install Beego
-    echo "Installing BeeGo v2"
-    go install github.com/beego/bee/v2@develop
+  # Install Beego
+  echo "Installing BeeGo v2"
+  go install github.com/beego/bee/v2@develop
 fi
 
 # Installing OpenVPN-UI and qrencode
@@ -123,10 +122,10 @@ read -p "Would you like to build OpenVPN-UI and qrencode binaries? (y/n) " -n 1 
 echo    # move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-    # Install OpenVPN-UI and qrencode
-    echo "Installing OpenVPN-UI and qrencode"
-    source ~/.bashrc # reload bashrc to get bee command
-  
+  # Install OpenVPN-UI and qrencode
+  echo "Installing OpenVPN-UI and qrencode"
+  source ~/.bashrc # reload bashrc to get bee command
+
   if [ -d "qrencode" ]; then
     echo "Directory 'qrencode' already exists. Skipping git clone."
   else
@@ -134,30 +133,30 @@ then
     git clone https://github.com/d3vilh/qrencode
   fi
 
-    # Set environment variables
-    export GO111MODULE='auto'
-    export CGO_ENABLED=1
-    export CC=musl-gcc 
+  # Set environment variables
+  export GO111MODULE='auto'
+  export CGO_ENABLED=1
+  export CC=musl-gcc 
 
-    # Packing openvpn-ui
+  # Packing openvpn-ui
   CURPATH=$(pwd)
   cd ../
-    echo "Building and packing OpenVPN-UI"
-    # Execute bee pack
-    export PATH=$PATH:$(go env GOPATH)/bin
-    go env -w GOFLAGS="-buildvcs=false"
-    source ~/.bashrc
-    bee version
-    bee pack -exr='^vendor|^ace.tar.bz2|^data.db|^build|^README.md|^docs'
+  echo "Building and packing OpenVPN-UI"
+  # Execute bee pack
+  export PATH=$PATH:$(go env GOPATH)/bin
+  go env -w GOFLAGS="-buildvcs=false"
+  source ~/.bashrc
+  bee version
+  bee pack -exr='^vendor|^ace.tar.bz2|^data.db|^build|^README.md|^docs'
 
-    # Build qrencode
-    echo "Building qrencode"
-    cd build/qrencode
-    go build -o qrencode main.go
-    chmod +x qrencode
-    echo "Moving qrencode to GOPATH"
-    mv -v qrencode $(go env GOPATH)/bin
-    cd $CURPATH
+  # Build qrencode
+  echo "Building qrencode"
+  cd build/qrencode
+  go build -o qrencode main.go
+  chmod +x qrencode
+  echo "Moving qrencode to GOPATH"
+  mv -v qrencode $(go env GOPATH)/bin
+  cd $CURPATH
 fi
 
 printf "\033[1;34mAll done.\033[0m\n"
